3372a4a1e555bcec4fcbf116d290f035768fad34
Lucas Andrade <lucasandrade_las@outlook.com>
2026-05-07 12:22:15 -0300

    Subida Spring Boot
    Primeira subida com os arquivos do spring boot

---
+++ projetofinanca/src/main/java/com/example/projetofinanca/ProjetofinancaApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetofinanca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetofinancaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetofinancaApplication.class, args);
+	}
+
+}
